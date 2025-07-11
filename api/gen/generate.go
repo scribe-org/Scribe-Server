cd97683a3f4f30e01c0cc5e0304b36966a950e55
Akindele Michael <akindelemichael65@gmail.com>
2025-07-11 09:56:31 +0100

    remove sqlc and openapi libraries

--- api/gen/generate.go
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-package api
-
-//go:generate oapi-codegen --config=server.cfg.yaml ../openapi.yaml
-//go:generate oapi-codegen --config=types.cfg.yaml ../openapi.yaml
