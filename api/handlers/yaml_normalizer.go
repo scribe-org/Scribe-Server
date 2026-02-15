c9b99b5aaf83b348bc9d055ac8518d155ef466df
Akindele Michael <49593618+DeleMike@users.noreply.github.com>
2026-02-15 23:39:19 +0100

    fix: change contracts from json to yaml (#56)
    * fix: change contracts from json to yaml
    
    * fix license issue
    
    * remove JSON files and revert server.go
    
    * Update contracts and add .gitkeep file for future issue
    
    * Comment wording correction
    
    * Minor fix in comment in contracts
    
    ---------
    
    Co-authored-by: Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>

---
+++ api/handlers/yaml_normalizer.go
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package handlers
+
+import "fmt"
+
+// Ensure all map types are converted to a standard map[string, any] format.
+func normalizeMap(i any) any {
+	switch x := i.(type) {
+
+	case map[string]any:
+		for k, v := range x {
+			x[k] = normalizeMap(v)
+		}
+		return x
+
+	case map[any]any:
+		m2 := map[string]any{}
+		for k, v := range x {
+			m2[fmt.Sprint(k)] = normalizeMap(v)
+		}
+		return m2
+
+	case []any:
+		for i, v := range x {
+			x[i] = normalizeMap(v)
+		}
+		return x
+	}
+
+	return i
+}
