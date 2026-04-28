99577af7766dcce05845a4bc2ef000704493525c
Lance Sigersmith <lancedjunior@gmail.com>
2026-04-28 10:33:19 -0400

    test english

--- update_data.sh
+++ update_data.sh
@@ -16,7 +16,7 @@ SKIP_MIGRATION=${1:-false}
 PROJECT_ROOT=$(pwd)
 
 # Define target languages and data types.
-TARGET_LANGUAGES=("english" "french" "german" "italian" "spanish" "portuguese" "russian" "swedish")
+TARGET_LANGUAGES=("english") #"french" "german" "italian" "spanish" "portuguese" "russian" "swedish")
 DATA_TYPES=("nouns" "verbs" "emoji_keywords")
 
 RED='\033[0;31m'
